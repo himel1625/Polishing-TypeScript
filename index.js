fa96045ee2c3c21e8293ca8c8b5fca299df568eb
himel1625 <himelmia1625@gmail.com>
2025-03-16 22:11:15 +0600

    Add tuple and object to store user data with logging

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-console.log('Hello TypeScript');
+// console.log('Hello TypeScript');
 // Arrays
-// const arr: Array<number> = [1, 2, 3, 4, 5];
-// console.log(arr);
-// const arr: number[] = [54, 654, 51, 5, 4, 54, 5];
-// console.log(arr);
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
+var arr = [54, 654, 51, 5, 4, 54, 5];
+console.log(arr);
+//Tuples
+var arr = ['Himel', 18];
+console.log(arr);
+var arrs = {
+    id: 2342,
+    Name: 'Himel',
+};
+console.log(arrs);
--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
-console.log('Hello TypeScript');
+// console.log('Hello TypeScript');
 
 // Arrays
-// const arr: Array<number> = [1, 2, 3, 4, 5];
-// console.log(arr);
-// const arr: number[] = [54, 654, 51, 5, 4, 54, 5];
-// console.log(arr);
+const arr: Array<number> = [1, 2, 3, 4, 5];
+console.log(arr);
+const arr: number[] = [54, 654, 51, 5, 4, 54, 5];
+console.log(arr);
+
+//Tuples
+let arr: [string, number] = ['Himel', 18];
+console.log(arr);
+const arrs: { id: Number; Name: string } = {
+   id: 2342,
+   Name: 'Himel',
+};
+console.log(arrs);
