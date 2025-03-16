02130900cf5828fb6ccd00a174cb94273e00d239
himel1625 <himelmia1625@gmail.com>
2025-03-16 22:04:33 +0600

    Log array of numbers to console for debugging

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
 console.log('Hello TypeScript');
+
+// Arrays
+// const arr: Array<number> = [1, 2, 3, 4, 5];
+// console.log(arr);
+// const arr: number[] = [54, 654, 51, 5, 4, 54, 5];
+// console.log(arr);
